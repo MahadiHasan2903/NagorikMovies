17ec8b4737e9c4b0e6583ba80c0af5665e30f53c
MD. Mahadi Hasan <12mahadihasan@gmail.com>
2024-10-26 20:42:43 +0600

    env updated

--- lib/config/constants.ts
+++ lib/config/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-export const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+export const BASE_URL = process.env.BASE_URL;
+export const TMDB_API_KEY = process.env.TMDB_API_KEY;
